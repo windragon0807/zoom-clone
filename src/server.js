d6ea8c6f99642eedddb59ab8c665f5cd989fa316
JSR <68853729+tmdfyd2020@users.noreply.github.com>
2022-07-11 17:08:49 +0900

    2022-07-11 lecture start

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug"); // pug로 view engine 설정
+app.set("views", __dirname + "/views"); // template이 어디 있는지 지정
+app.use("/public", express.static(__dirname + "/public")); // public(frontend) 폴더를 유저에게 공개
+app.get("/", (_, res) => res.render("home")); // home.pug를 render 해주는 route handler 생성
+app.get("/*", (_, res) => res.redirect("/")); // home으로 리다이렉팅
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
+
+// express는 ws를 지원하지 않기 때문에 새로운 function을 추가해야 한다.
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server }); // http 서버 위에 webSocket 서버 만들 수 있음
+
+// fake database
+const sockets = [];
+
+// wss.on은 이벤트가 발생할 때까지 기다린다.
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser!!");
+
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+
+  socket.on("close", () => console.log("Disconnect from the Browser"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
